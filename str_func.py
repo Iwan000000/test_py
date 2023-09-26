f8d91cea9d6e4308add4c85b849afe7247fbf95e
Iwan000000 <145742711+Iwan000000@users.noreply.github.com>
2023-09-27 01:45:27 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,10 +1,3 @@
 def uppercase_string(input_string):
     '''Преобразует строку в верхний регистр.'''
     return input_string.upper()
-
-
-def capitalize_words(input_string):
-    """функция делает заглавными первые буквы каждого слова в строке, поступившей на вход функции."""
-    words = input_string.split()
-    capitalized_words = [word.capitalize() for word in words]
-    return ' '.join(capitalized_words)
