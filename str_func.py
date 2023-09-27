b2e373d4d3eb810dc82c94101fd518d4da387f64
Iwan000000 <145742711+Iwan000000@users.noreply.github.com>
2023-09-27 23:24:48 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,3 @@
 def uppercase_string(input_string):
-        '''Функция, которая принимает на вход строку и возвращает ее со всеми заглавными буквами'''
+        '''Функция'''
     return input_string.upper()
